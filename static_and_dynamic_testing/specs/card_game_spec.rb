1ac6c6de8755c3c7c81d57ddb5ce37ee41be0ca1
Gillian Shanks <GillianShanks88@gmail.com>
2019-07-25 15:19:04 +0100

    Adds spec dynamic testing for Card class, testing can get suit and value.

--- static_and_dynamic_testing/specs/card_game_spec.rb
+++ static_and_dynamic_testing/specs/card_game_spec.rb
@@ -1,5 +1,6 @@
 require("minitest/autorun")
 require("minitest/rg")
+require_relative("../card_game.rb")
 require_relative("../card.rb")
 
 class CardGameTest < MiniTest::Test
--- static_and_dynamic_testing/specs/card_spec.rb
+++ static_and_dynamic_testing/specs/card_spec.rb
@@ -1,9 +1,18 @@
 require("minitest/autorun")
 require("minitest/rg")
-require_relative("../card_game.rb")
 require_relative("../card.rb")
 
 class CardTest < MiniTest::Test
+  def setup()
+    @card1 = Card.new("heart", 3)
+  end
 
+  def test_can_get_card_suit()
+    assert_equal("heart", @card1.suit)
+  end
+
+  def test_can_get_card_value()
+    assert_equal(3, @card1.value)
+  end
 
 end
